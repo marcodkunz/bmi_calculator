6ac9c95b103cd2c96b7577d55f127a689aa8e13b
Marco Kunz <marco@fivenine.ch>
2022-07-02 12:02:08 +0200

    adding home view test

--- test/helper/mock_implementations.dart
+++ test/helper/mock_implementations.dart
@@ -1,6 +1,7 @@
 import 'package:bmi_calculator/service/database_service.dart';
 import 'package:bmi_calculator/service/navigation/navigation_service.dart';
 import 'package:bmi_calculator/viewmodel/history_view_model.dart';
+import 'package:bmi_calculator/viewmodel/home_view_model.dart';
 import 'package:bmi_calculator/viewmodel/splash_view_model.dart';
 import 'package:mockito/annotations.dart';
 
@@ -11,10 +12,12 @@ import 'mock_implementations.mocks.dart';
   INavigationService,
   SplashViewModel,
   HistoryViewModel,
+  HomeViewModel,
 ])
 class Mocks {
   MockIDatabaseService get IDatabaseService => MockIDatabaseService();
   MockINavigationService get INavigationService => MockINavigationService();
   MockSplashViewModel get SplashViewModel => MockSplashViewModel();
   MockHistoryViewModel get HistoryViewModel => MockHistoryViewModel();
+  MockHomeViewModel get HomeViewModel => MockHomeViewModel();
 }
